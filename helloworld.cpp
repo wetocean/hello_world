aba435e829505268929784d778660bee571e3d7e
Ibrahim Kazanci <67881867+wetocean@users.noreply.github.com>
2024-02-27 18:48:33 +0000

    Add helloworld for C++ and PHP

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\n";
+  return 0;
+}
+
+// Open a terminal and run the following commands
+// $ g++ helloworld.cpp -o helloworld
+// $ ./helloworld
+// Output: Hello World!
---
+++ helloworld.php
@@ -0,0 +1,7 @@
+<?php
+Print "Hello, World!\n";
+?>
+
+// Open a terminal and run the following command
+// $ php helloworld.php
+// Output: Hello, World!
