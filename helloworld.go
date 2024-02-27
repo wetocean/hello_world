bfd0ecc767915219e2825e0bbd3705dac81468a6
Ibrahim Kazanci <67881867+wetocean@users.noreply.github.com>
2024-02-27 18:39:23 +0000

    Add helloworld for go & JS

---
+++ helloworld.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+func main() {
+    fmt.Println("Hello World!")
+}
+
+// Open Terminal
+// go run helloworld.go
+// Output: Hello World!
---
+++ helloworld.js
@@ -0,0 +1,5 @@
+console.log("Hello World!");
+
+// Open a terminal and run the following command:
+// node helloworld.js
+// Output: Hello World!
